6d38c54edb21e512a446ac2e027b59881799d7d0
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-25 19:52:38 +0100

    Added remaining modals

--- public/js/index.js
+++ public/js/index.js
@@ -68,76 +68,76 @@ var form2 = {
     price: 8000
 }
 
-// // form 3
-// var shoe3Form = document.forms.shoe3Form;
-// var form3 = {
-//     inputs: document.querySelectorAll('.form3Inputs'),
-//     color: shoe3Form.shoe3Color,
-//     button: document.querySelector('#shoe3Submit'),
-//     total: document.querySelector('#shoe3Total'),
-//     price: 15000
-// }
+// form 3
+var shoe3Form = document.forms.shoe3Form;
+var form3 = {
+    inputs: document.querySelectorAll('.form3Inputs'),
+    color: shoe3Form.shoe3Color,
+    button: document.querySelector('#shoe3Submit'),
+    total: document.querySelector('#shoe3Total'),
+    price: 15000
+}
 
-// // form 4
-// var shoe4Form = document.forms.shoe4Form;
-// var form4 = {
-//     inputs: document.querySelectorAll('.form4Inputs'),
-//     color: shoe4Form.shoe4Color,
-//     button: document.querySelector('#shoe4Submit'),
-//     total: document.querySelector('#shoe4Total'),
-//     price: 3500
-// }
+// form 4
+var shoe4Form = document.forms.shoe4Form;
+var form4 = {
+    inputs: document.querySelectorAll('.form4Inputs'),
+    color: shoe4Form.shoe4Color,
+    button: document.querySelector('#shoe4Submit'),
+    total: document.querySelector('#shoe4Total'),
+    price: 3500
+}
 
-// // form 5
-// var shoe5Form = document.forms.shoe5Form;
-// var form5 = {
-//     inputs: document.querySelectorAll('.form5Inputs'),
-//     color: shoe5Form.shoe5Color,
-//     button: document.querySelector('#shoe5Submit'),
-//     total: document.querySelector('#shoe5Total'),
-//     price: 10000
-// }
+// form 5
+var shoe5Form = document.forms.shoe5Form;
+var form5 = {
+    inputs: document.querySelectorAll('.form5Inputs'),
+    color: shoe5Form.shoe5Color,
+    button: document.querySelector('#shoe5Submit'),
+    total: document.querySelector('#shoe5Total'),
+    price: 10000
+}
 
-// // form 6
-// var shoe6Form = document.forms.shoe6Form;
-// var form6 = {
-//     inputs: document.querySelectorAll('.form6Inputs'),
-//     color: shoe6Form.shoe6Color,
-//     button: document.querySelector('#shoe6Submit'),
-//     total: document.querySelector('#shoe6Total'),
-//     price: 15000
-// }
+// form 6
+var shoe6Form = document.forms.shoe6Form;
+var form6 = {
+    inputs: document.querySelectorAll('.form6Inputs'),
+    color: shoe6Form.shoe6Color,
+    button: document.querySelector('#shoe6Submit'),
+    total: document.querySelector('#shoe6Total'),
+    price: 15000
+}
 
-// // form 7
-// var shoe7Form = document.forms.shoe7Form;
-// var form7 = {
-//     inputs: document.querySelectorAll('.form7Inputs'),
-//     color: shoe7Form.shoe7Color,
-//     button: document.querySelector('#shoe7Submit'),
-//     total: document.querySelector('#shoe7Total'),
-//     price: 10000
-// }
+// form 7
+var shoe7Form = document.forms.shoe7Form;
+var form7 = {
+    inputs: document.querySelectorAll('.form7Inputs'),
+    color: shoe7Form.shoe7Color,
+    button: document.querySelector('#shoe7Submit'),
+    total: document.querySelector('#shoe7Total'),
+    price: 10000
+}
 
-// // form 8
-// var shoe8Form = document.forms.shoe8Form;
-// var form8 = {
-//     inputs: document.querySelectorAll('.form8Inputs'),
-//     color: shoe8Form.shoe1Color,
-//     button: document.querySelector('#shoe8Submit'),
-//     total: document.querySelector('#shoe8Total'),
-//     price: 8000
-// }
+// form 8
+var shoe8Form = document.forms.shoe8Form;
+var form8 = {
+    inputs: document.querySelectorAll('.form8Inputs'),
+    color: shoe8Form.shoe1Color,
+    button: document.querySelector('#shoe8Submit'),
+    total: document.querySelector('#shoe8Total'),
+    price: 8000
+}
 
 
-// // form 9
-// var shoe9Form = document.forms.shoe9Form;
-// var form9 = {
-//     inputs: document.querySelectorAll('.form9Inputs'),
-//     color: shoe9Form.shoe9Color,
-//     button: document.querySelector('#shoe9Submit'),
-//     total: document.querySelector('#shoe9Total'),
-//     price: 25000
-// }
+// form 9
+var shoe9Form = document.forms.shoe9Form;
+var form9 = {
+    inputs: document.querySelectorAll('.form9Inputs'),
+    color: shoe9Form.shoe9Color,
+    button: document.querySelector('#shoe9Submit'),
+    total: document.querySelector('#shoe9Total'),
+    price: 25000
+}
 
 function addQuantityEventListener(form) {
     form.inputs[3].addEventListener('keyup', function (event) {
@@ -200,9 +200,9 @@ function validate (form) {
                     html: 'Please provide a valid input'
                 });
                 inputs[i][0].focus();
+                break;
             }
         }, false);
-        break;
     }
 }
 
@@ -216,37 +216,37 @@ addButtonEventListener(form2.button, form2);
 addQuantityEventListener(form2);
 validate(form2);
 
-// //Form 3
-// addButtonEventListener(form3.button, form3);
-// addQuantityEventListener(form3);
-// validate(form3);
+//Form 3
+addButtonEventListener(form3.button, form3);
+addQuantityEventListener(form3);
+validate(form3);
 
-// //Form 4
-// addButtonEventListener(form4.button, form4);
-// addQuantityEventListener(form4);
-// validate(form4);
+//Form 4
+addButtonEventListener(form4.button, form4);
+addQuantityEventListener(form4);
+validate(form4);
 
-// //Form 5
-// addButtonEventListener(form5.button, form5);
-// addQuantityEventListener(form5);
-// validate(form5);
+//Form 5
+addButtonEventListener(form5.button, form5);
+addQuantityEventListener(form5);
+validate(form5);
 
-// //Form 6
-// addButtonEventListener(form6.button, form6);
-// addQuantityEventListener(form6);
-// validate(form6);
+//Form 6
+addButtonEventListener(form6.button, form6);
+addQuantityEventListener(form6);
+validate(form6);
 
-// //Form 7
-// addButtonEventListener(form7.button, form7);
-// addQuantityEventListener(form7);
-// validate(form7);
+//Form 7
+addButtonEventListener(form7.button, form7);
+addQuantityEventListener(form7);
+validate(form7);
 
-// //Form 8
-// addButtonEventListener(form8.button, form8);
-// addQuantityEventListener(form8);
-// validate(form8);
+//Form 8
+addButtonEventListener(form8.button, form8);
+addQuantityEventListener(form8);
+validate(form8);
 
-// //Form 9
-// addButtonEventListener(form9.button, form9);
-// addQuantityEventListener(form9);
-// validate(form9);
+//Form 9
+addButtonEventListener(form9.button, form9);
+addQuantityEventListener(form9);
+validate(form9);
